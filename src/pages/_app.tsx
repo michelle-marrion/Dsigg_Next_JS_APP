c89cee0b75e82e830affc590e4d8bacd32ee35d0
michelle-marrion <ngamimarrion@gmail.com>
2023-12-06 09:58:05 +0100

    add files of theme building and roouting of application. And file of home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,65 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { ReactElement, ReactNode } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Router from 'next/router';
+import nProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import ThemeProvider from '@/styles/theme/ThemeProvider';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import createEmotionCache from '@/createEmotionCache';
+import {SidebarProvider} from  '@/app/components/widgets/contexts/SidebarContext'
+//import SidebarProvider from '@/app/components/widgets/layouts/SidebarLayout';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+
+const clientSideEmotionCache = createEmotionCache();
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+interface TokyoAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+  Component: NextPageWithLayout;
 }
+
+function TokyoApp(props: TokyoAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  Router.events.on('routeChangeStart', nProgress.start);
+  Router.events.on('routeChangeError', nProgress.done);
+  Router.events.on('routeChangeComplete', nProgress.done);
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Tokyo Free White NextJS Typescript Admin Dashboard</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <SidebarProvider>
+        <ThemeProvider>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <CssBaseline />
+            {getLayout(<Component {...pageProps} />)}
+          </LocalizationProvider>
+        </ThemeProvider>
+      </SidebarProvider>
+    </CacheProvider>
+  );
+}
+
+export default TokyoApp;
+
+import 'src/styles/globals.css'
+// import type { AppProps } from 'next/app'
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
