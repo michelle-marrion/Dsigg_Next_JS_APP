9e80e5ee5cd82ce488d4a5dee6580957967ce2b9
michelle-marrion <ngamimarrion@gmail.com>
2023-12-13 14:39:52 +0100

    Implement NavBar_Link on dashboards/home page

--- src/pages/dashboards/home/index.tsx
+++ src/pages/dashboards/home/index.tsx
@@ -1,21 +1,66 @@
 import Head from 'next/head';
-
+import Link from 'next/link';
+import {useRouter} from 'next/router';
 import SidebarLayout from '@/app/components/widgets/layouts/SidebarLayout';
 
 import PageHeader from '@/app/components/pages/Dashboards/Home/PageHeader';
 import PageTitleWrapper from '@/app/components/shared/PageTitleWrapper';
 import { Container, Grid } from '@mui/material';
 import Footer from '@/app/components/shared/Footer';
-import AccountBalance from '@/app/components/pages/Dashboards/Home/AccountBalance';
+import RecentOrders from '@/app/components/pages/Management/Transactions/RecentOrders'
+
+/**Import Icon */
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded'
+import ChevronRightTwoToneIcon from '@mui/icons-material/ChevronRightTwoTone';
+function NavBar() {
+  const router = useRouter()
+  return (
+    <nav>
+      <Link href="/" className={router.pathname === '/' ? 'selected' :''} style={linkStyle('Home')}>
+       <HomeRoundedIcon /> 
+      </Link> <ChevronRightTwoToneIcon/>
+      <Link href="/dashboards/home" className={router.pathname === '/dashboards/home' ? 'selected' :''} style={linkStyle('List')}>
+        Dashboard
+      </Link>
+      <style jsx>{`
+        nav {
+          background-color:;
+          text-align: left;
+          margin-top : 20px;
+          margin-bottom : 20px;
+        }
+        Link {
+          margin-left:-10px;
+          text-decoration: none;
+          color: black;
+          padding: 1px;
+        }
+        .selected {
+          border-bottom: 2px solid red;
+        }
+      `}</style>
+    </nav>
+  );
+}
+
+function linkStyle(page) {
+  return {
+    marginLeft: page === 'home' ? 0 : 20,
+    display: page === 'home' ? 'none' : 'inline-block',
+    fontWeight: page === 'home' ? 'bold' : 'normal',
+  };
+}
 function DashboardCrypto() {
   return (
     <>
       <Head>
         <title>Crypto Dashboard</title>
       </Head>
+      <NavBar/> 
       <PageTitleWrapper>
         <PageHeader />
       </PageTitleWrapper>
+     
       {/* <TopBarContent/> */}
       <Container maxWidth="lg">
         <Grid
@@ -23,10 +68,10 @@ function DashboardCrypto() {
           direction="row"
           justifyContent="center"
           alignItems="stretch"
-          spacing={4}
+          spacing={3}
         >
           <Grid item xs={12}>
-            <AccountBalance />
+            <RecentOrders />
           </Grid>
         </Grid>
       </Container>
