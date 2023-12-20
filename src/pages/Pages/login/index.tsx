059b7e970554e771a9153886fbc0bcbc969668b9
michelle-marrion <ngamimarrion@gmail.com>
2023-12-20 19:35:36 +0100

    Element of Other Feature : NavBarLink and build App

--- src/pages/Pages/login/index.tsx
+++ src/pages/Pages/login/index.tsx
@@ -1,14 +1,19 @@
 
 import type { ReactElement } from 'react';
 import BaseLayout from 'src/app/components/widgets/layouts/BaseLayout';
-
-import LoginForm from '@/app/components/pages/Authentification/LoginForm'
+//import AuthorForm from 'src/app/components/shared/Forms'
+//import ImageUpload from '@/app/components/shared/Forms/Image/ManyImage';
+import FileUpload from '@/app/components/shared/Forms/Files/FileUpload'
+//import LoginForm from '@/app/components/pages/Authentification/LoginForm'
 import Footer from '@/app/components/shared/Footer';
 
 function Overview() {
   return (
   <>
-      <LoginForm/>
+      {/* <LoginForm/> */}
+      {/* <AuthorForm/> */}
+      {/* <ImageUpload/> */}
+      <FileUpload/>
       <Footer/>
   </>
   );
